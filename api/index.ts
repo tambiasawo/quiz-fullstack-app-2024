4b76c8bd7e210009d8e01378fdf089387d3bb844
Tambi <tambi.asawo@verticalmotion.ca>
2024-05-31 12:58:39 -0700

    feat: create new node app

---
+++ api/index.ts
@@ -0,0 +1,29 @@
+import dotenv from "dotenv";
+dotenv.config();
+import cors from "cors";
+import QuizRouter from "./routes/quiz.route";
+import AuthRouter from "./routes/auth.route";
+import express from "express";
+import dbConnect from "./utils/dbConnect";
+import errorHandler from "./utils/errorHandler";
+import cookieParser from "cookie-parser";
+import ScoreRouter from "./routes/score.route";
+dbConnect();
+
+const PORT = 3000;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+app.use(`/api/quiz`, QuizRouter);
+app.use(`/api/auth`, AuthRouter);
+app.use("/api/scores", ScoreRouter);
+app.use(errorHandler);
+app.get("/", (req, res) => {
+  return res.send("hello");
+});
+
+app.listen(PORT, () => {
+  console.log("listening on port " + PORT);
+});
