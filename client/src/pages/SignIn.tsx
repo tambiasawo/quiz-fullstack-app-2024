df099cc0e6377fafc411f5e9b9603a9cd6cda7ea
Tambi <tambi.asawo@verticalmotion.ca>
2024-05-31 13:11:34 -0700

    feat:add auth pages

--- client/src/pages/SignIn.tsx
+++ client/src/pages/SignIn.tsx
@@ -1,8 +1,135 @@
+import React, { ChangeEvent, FormEvent } from "react";
+import { HiOutlineMail } from "react-icons/hi";
+import { Link, useNavigate } from "react-router-dom";
+import { IoEyeOutline } from "react-icons/io5";
+import { signIn } from "../store/features/auth/authSlice";
+import { useDispatch } from "react-redux";
+
 export const SignIn = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [loading, setLoading] = React.useState(false);
+  const [error, setError] = React.useState("");
+
+  const [viewPassword, setViewPassword] = React.useState(false);
+  const [userInfo, setUserInfo] = React.useState({
+    username: "",
+    password: "",
+  });
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setError("");
+    setUserInfo((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!userInfo.password || !userInfo.password) return;
+
+    try {
+      setLoading(true);
+      const response = await fetch(`/api/auth/signin`, {
+        method: "POST",
+        body: JSON.stringify(userInfo),
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const data = await response.json();
+      console.log(data);
+
+      if (data.user && data.success) {
+        dispatch(signIn(data.user));
+        setLoading(false);
+        navigate("/");
+        return;
+      } else {
+        setLoading(false);
+        setError(data.message);
+      }
+    } catch (err) {
+      setLoading(false);
+      setError("Something unexpected occured. Please try again");
+    }
+  };
+
+  const [emailIconColor, setEmailIconColor] = React.useState("");
+  const [passwordIconColor, setPasswordIconColor] = React.useState("");
+
   return (
-    <section>
-      <input type="text" name="username" placeholder="Username" />
-      <input type="password" name="password" placeholder="password" />
-    </section>
+    <div className="my-40 bg-white max-w-md mx-auto p-9 rounded-lg">
+      <h3 className="text-center text-2xl text-black mb-5 font-semibold">
+        Log In
+      </h3>
+      <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+        <span className="flex justify-between items-center form-control-sign-in">
+          <input
+            type="text"
+            name="username"
+            onChange={handleChange}
+            placeholder="Username"
+            className="form-control-sign-in w-full text-black"
+            onFocus={() =>
+              setEmailIconColor((prev) => {
+                if (prev) return "";
+                return "text-black";
+              })
+            }
+            onBlur={() => setEmailIconColor("")}
+            required
+          />
+          <HiOutlineMail
+            size={20}
+            className={`${emailIconColor} hover:${emailIconColor}`}
+          />
+        </span>
+        <span className="flex justify-between items-center form-control-sign-in">
+          <input
+            type={viewPassword ? "text" : "password"}
+            name="password"
+            placeholder="Password"
+            onChange={handleChange}
+            className="form-control-sign-in w-full text-black"
+            onFocus={() =>
+              setPasswordIconColor((prev) => {
+                if (prev) return "";
+                return "text-black";
+              })
+            }
+            onBlur={() => setPasswordIconColor("")}
+            required
+          />
+          <IoEyeOutline
+            size={20}
+            className={`${passwordIconColor} cursor-pointer hover:${passwordIconColor}`}
+            onClick={() => setViewPassword((prev) => !prev)}
+          />
+        </span>
+        <button className="text-black text-center bg-[#fe9d73] rounded-xl py-2 text-lg hover:opacity-[.9]">
+          {loading ? "Signing In" : "Sign In"}
+        </button>
+
+        <button className="text-center bg-[#fe9d73] rounded-xl py-2 text-lg hover:opacity-[.9]">
+          <span className="flex text-black items-center justify-center gap-3">
+            Continue With Google{" "}
+            <img
+              src="/assets/google.svg"
+              alt="google sign in"
+              className="w-6 h-6"
+            />
+          </span>
+        </button>
+        <span className="text-center text-black">
+          Don't Have an Account?{" "}
+          <Link to="/sign-up" className="underline cursor-pointer">
+            {" "}
+            Sign up
+          </Link>
+        </span>
+        <p className="text-red-500 text-center">{error}</p>
+      </form>
+    </div>
   );
 };
---
+++ client/src/pages/SignUp.tsx
@@ -0,0 +1,204 @@
+import React, { ChangeEvent, FormEvent } from "react";
+import { HiOutlineMail, HiOfficeBuilding, HiUserCircle } from "react-icons/hi";
+import { IoEyeOutline } from "react-icons/io5";
+import { Link, useNavigate } from "react-router-dom";
+import { signIn } from "../store/features/auth/authSlice";
+import { useDispatch } from "react-redux";
+
+export const SignUp = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [viewPassword, setViewPassword] = React.useState(false);
+  const [error, setError] = React.useState("");
+  const [loading, setLoading] = React.useState(false);
+  const [values, setValues] = React.useState({
+    name: "",
+    company: "",
+    username: "",
+    email: "",
+    password: "",
+    cpassword: "",
+  });
+  console.log({ values });
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setError("");
+    console.log(e.target.name);
+    setValues((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const handleForm = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log({ values });
+
+    if (!values.name || !values.email || !values.username || !values.password)
+      return;
+    console.log("rer");
+
+    /*  if (values.password !== values.cpassword) {
+      setError("Passwords must match");
+      return;
+    } */
+    const { cpassword, ...rest } = values;
+    setLoading(true);
+    try {
+      const response = await fetch(`/api/auth/signup`, {
+        method: "POST",
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(rest),
+      });
+
+      const data = await response.json();
+      console.log(data);
+      if (data.success) {
+        setLoading(false);
+        dispatch(signIn(data.user));
+        navigate("/");
+      } else {
+        setLoading(false);
+        setError(data.message);
+      }
+    } catch (e) {
+      setLoading(false);
+      setError("Somethign went wrong. Please try again");
+      console.log(e);
+    }
+  };
+
+  const handleIconColorChange = (e) => {
+    if (e.type === "focus") {
+      e.target.nextElementSibling.classList.add("active-field");
+    } else {
+      e.target.nextElementSibling.classList.remove("active-field");
+    }
+  };
+
+  return (
+    <div className="my-10 bg-white max-w-md mx-auto p-9 rounded-lg">
+      <h3 className="text-center text-2xl text-black mb-5 font-semibold">
+        Sign Up{" "}
+      </h3>
+      <form className="flex flex-col gap-4" onSubmit={handleForm}>
+        <span className="flex justify-between items-center form-control-sign-in">
+          <input
+            type="text"
+            name="name"
+            onChange={handleChange}
+            placeholder="Name*"
+            className="form-control-sign-in w-full"
+            onFocus={(e) => handleIconColorChange(e)}
+            onBlur={(e) => handleIconColorChange(e)}
+            required
+          />
+          <HiUserCircle size={20} />
+        </span>
+        <span className="flex justify-between items-center form-control-sign-in">
+          <input
+            type="text"
+            name="company"
+            placeholder="Company"
+            onChange={handleChange}
+            className="form-control-sign-in w-full"
+            onFocus={(e) => handleIconColorChange(e)}
+            onBlur={(e) => handleIconColorChange(e)}
+          />
+          <HiOfficeBuilding size={20} />
+        </span>
+        <span className="flex justify-between items-center form-control-sign-in">
+          <input
+            type="text"
+            name="username"
+            placeholder="Username*"
+            onChange={handleChange}
+            required
+            className="form-control-sign-in w-full"
+            onFocus={(e) => handleIconColorChange(e)}
+            onBlur={(e) => handleIconColorChange(e)}
+          />
+          <HiUserCircle size={20} />
+        </span>
+        <span className="flex justify-between items-center form-control-sign-in">
+          <input
+            type="email"
+            name="email"
+            placeholder="Email Address*"
+            required
+            onChange={handleChange}
+            className="form-control-sign-in w-full"
+            onFocus={(e) => handleIconColorChange(e)}
+            onBlur={(e) => handleIconColorChange(e)}
+          />
+          <HiOutlineMail size={20} className={` hover:text-[#fe9d73]`} />
+        </span>
+        <span className="flex justify-between items-center form-control-sign-in">
+          <input
+            type={viewPassword ? "text" : "password"}
+            name="password"
+            placeholder="Password*"
+            required
+            onChange={handleChange}
+            className="form-control-sign-in w-full"
+            onFocus={(e) => handleIconColorChange(e)}
+            onBlur={(e) => handleIconColorChange(e)}
+          />
+
+          <IoEyeOutline
+            size={20}
+            className={` cursor-pointer hover:text-[#fe9d73]`}
+            onClick={() => setViewPassword((prev) => !prev)}
+          />
+        </span>
+        <span className="flex justify-between items-center form-control-sign-in">
+          <input
+            type={viewPassword ? "text" : "password"}
+            name="cpassword"
+            required
+            placeholder="Confirm Password*"
+            className="form-control-sign-in w-full"
+            onFocus={(e) => handleIconColorChange(e)}
+            onBlur={(e) => handleIconColorChange(e)}
+          />
+
+          <IoEyeOutline
+            size={20}
+            className={`cursor-pointer hover:text-[#fe9d73]`}
+            onClick={() => setViewPassword((prev) => !prev)}
+          />
+        </span>
+
+        <button
+          type="submit"
+          disabled={loading}
+          className="text-black text-center bg-[#fe9d73] rounded-xl py-2 text-lg hover:opacity-[.9]"
+        >
+          Sign Up
+        </button>
+
+        <button
+          type="button"
+          className="text-black text-center bg-[#fe9d73] rounded-xl py-2 text-lg hover:opacity-[.9]"
+        >
+          <span className="flex items-center justify-center gap-3">
+            Continue With Google{" "}
+            <img
+              src="/assets/google.svg"
+              alt="google sign in"
+              className="w-6 h-6"
+            />
+          </span>
+        </button>
+        <span className="text-black text-center">
+          Already Have an Account?{" "}
+          <Link to="/sign-in" className="underline">
+            {" "}
+            Sign in
+          </Link>
+          <p className="text-red-500">{error}</p>
+        </span>
+      </form>
+    </div>
+  );
+};
