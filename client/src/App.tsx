6ea3f5f5d025fec41d36ad8051f6676f9ddba318
Tambi <tambi.asawo@verticalmotion.ca>
2024-06-05 09:13:17 -0700

    cleanup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,7 @@
-import React from "react";
-import { useState } from "react";
-
 import Dashboard from "./pages/Dashboard";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <Dashboard />;
+  return "";
 }
 
 export default App;
