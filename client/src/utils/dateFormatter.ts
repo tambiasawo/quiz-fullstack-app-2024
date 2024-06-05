38f11398e0747130ab98cda4230df889c76abd38
Tambi <tambi.asawo@verticalmotion.ca>
2024-06-05 09:14:05 -0700

    feat: add date util func

---
+++ client/src/utils/dateFormatter.ts
@@ -0,0 +1,12 @@
+const dateFormatter = (date: string) => {
+  const d = new Date(date);
+  const year = d.getFullYear();
+  const month = String(d.getMonth() + 1).padStart(2, "0");
+  const day = String(d.getDate()).padStart(2, "0");
+  const hour = String(d.getHours()).padStart(2, "0");
+  const minute = String(d.getMinutes()).padStart(2, "0");
+
+  return `${year}-${month}-${day}  ${hour}:${minute}`;
+};
+
+export default dateFormatter;
