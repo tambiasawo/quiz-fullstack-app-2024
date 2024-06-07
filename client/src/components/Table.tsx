af2ddd97d78e9003df98d5104c0ef49ac09d3f49
Tambi <tambi.asawo@verticalmotion.ca>
2024-06-06 21:27:46 -0700

    feat: use mui table

--- client/src/components/Table.tsx
+++ client/src/components/Table.tsx
@@ -1,137 +1,116 @@
-import * as React from "react";
-import Paper from "@mui/material/Paper";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TablePagination from "@mui/material/TablePagination";
-import TableRow from "@mui/material/TableRow";
+"use client";
+import Box from "@mui/material/Box";
+import { DataGrid, GridColDef, GridRenderCellParams } from "@mui/x-data-grid";
 
-interface Column {
-  id: "name" | "code" | "population" | "size" | "density";
-  label: string;
-  minWidth?: number;
-  align?: "right";
-  format?: (value: number) => string;
+type Data<T> = T;
+interface Props {
+  rows: Data<Rows>[];
+  columns: GridColDef[];
+  title?: string;
+}
+export interface Rows {
+  id: number;
+  lastName: string;
+  firstName: string;
+  age: number;
+  username: string;
+  email: string;
+  gender: string;
+  lastlogin: Date;
 }
 
-const columns: readonly Column[] = [
-  { id: "name", label: "Name", minWidth: 170 },
-  { id: "code", label: "ISO\u00a0Code", minWidth: 100 },
-  {
-    id: "population",
-    label: "Population",
-    minWidth: 170,
-    align: "right",
-    format: (value: number) => value.toLocaleString("en-US"),
-  },
-  {
-    id: "size",
-    label: "Size\u00a0(km\u00b2)",
-    minWidth: 170,
-    align: "right",
-    format: (value: number) => value.toLocaleString("en-US"),
-  },
-  {
-    id: "density",
-    label: "Density",
-    minWidth: 170,
-    align: "right",
-    format: (value: number) => value.toFixed(2),
-  },
-];
+const DataTable = ({ rows, columns, title }: Props) => {
+  let updatedTableColumns = [...columns];
+  /*  if (user?.isAdmin) {
+    let action = deleteProduct;
+    let path = "products";
+    updatedTableColumns.push({
+      field: "action",
+      headerName: "Action",
+      headerAlign: "center",
+      align: "center",
+      width: 170,
+      renderCell: (params: GridRenderCellParams) => {
+        if (params.row.email) {
+          action = deleteUser;
+          path = "users";
+        } else if (params.row.invoice) {
+          action = deleteTransaction;
+          path = "transactions";
+        }
+        return (
+          <span className="flex justify-between gap-4">
+            <Link href={`/${path}/${params.id}`}>
+              <button className="bg-green-500 px-3 py-1 rounded-md">
+                View
+              </button>
+            </Link>
 
-interface Data {
-  name: string;
-  code: string;
-  population: number;
-  size: number;
-  density: number;
-}
-
-function createData(
-  name: string,
-  code: string,
-  population: number,
-  size: number
-): Data {
-  const density = population / size;
-  return { name, code, population, size, density };
-}
-
-const rows = [
-  createData("India", "IN", 1324171354, 3287263),
-  createData("China", "CN", 1403500365, 9596961),
-  createData("Italy", "IT", 60483973, 301340),
-];
-
-export default function StickyHeadTable() {
-  const [page, setPage] = React.useState(0);
-  const [rowsPerPage, setRowsPerPage] = React.useState(10);
-
-  const handleChangePage = (event: unknown, newPage: number) => {
-    setPage(newPage);
-  };
-
-  const handleChangeRowsPerPage = (
-    event: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    setRowsPerPage(+event.target.value);
-    setPage(0);
-  };
+            <form action={action}>
+              <input type="hidden" value={params.id} name="id" />
+              <button className="bg-red-500 px-3 py-1 rounded-md" type="submit">
+                Delete
+              </button>
+            </form>
+          </span>
+        );
+      },
+    });
+  } */
+/*   updatedTableColumns.forEach((column) => {
+    if (column.field === "createdAt") {
+      column.renderCell = (params: GridRenderCellParams) => {
+        const date = new Date(params.row.createdAt).toDateString();
+        if (date === "Invalid Date") return "Unknown Date";
+        return <span>{new Date(params.row.createdAt).toDateString()}</span>;
+      };
+    }
+    if (column.field === "isAdmin") {
+      column.renderCell = (params: GridRenderCellParams) => {
+        return <span>{params.row.isAdmin ? "Administrator" : "User"}</span>;
+      };
+    }
+    if (column.field === "isActive") {
+      column.renderCell = (params: GridRenderCellParams) => {
+        return <span>{params.row.isActive ? "Active" : "Inactive"}</span>;
+      };
+    }
+  }); */
 
   return (
-    <Paper
-      sx={{ width: "100%", overflow: "hidden", bgcolor: "#37373e" }}
-      className=""
-    >
-      <TableContainer sx={{ maxHeight: 440 }}>
-        <Table stickyHeader aria-label="sticky table">
-          <TableHead>
-            <TableRow>
-              {columns.map((column) => (
-                <TableCell
-                  key={column.id}
-                  align={column.align}
-                  style={{ minWidth: column.minWidth }}
-                >
-                  {column.label}
-                </TableCell>
-              ))}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {rows
-              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-              .map((row) => {
-                return (
-                  <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
-                    {columns.map((column) => {
-                      const value = row[column.id];
-                      return (
-                        <TableCell key={column.id} align={column.align}>
-                          {column.format && typeof value === "number"
-                            ? column.format(value)
-                            : value}
-                        </TableCell>
-                      );
-                    })}
-                  </TableRow>
-                );
-              })}
-          </TableBody>
-        </Table>
-      </TableContainer>
-      <TablePagination
-        rowsPerPageOptions={[10, 25, 100]}
-        component="div"
-        count={rows.length}
-        rowsPerPage={rowsPerPage}
-        page={page}
-        onPageChange={handleChangePage}
-        onRowsPerPageChange={handleChangeRowsPerPage}
-      />
-    </Paper>
+    <>
+      <h1 className="p-2 text-lg font-bold">{title}</h1>
+      <Box
+        sx={{ height: 500, width: "100%" }}
+        className="bg-softBg overflow-y-scroll"
+      >
+        <DataGrid
+          getRowId={(row) => row._id}
+          sx={{
+            color: "var(--textSoft)",
+            ".MuiTablePagination-displayedRows": { color: "var(--text)" },
+            ".MuiTablePagination-actions": { color: "var(--text)" },
+            ".MuiDataGrid-columnHeaders": {
+              backgroundColor: "var(--mainBg)",
+              fontSize: "16px",
+              fontWeight: "900",
+              color: "rgb(183, 186, 193)",
+            },
+          }}
+          rows={rows}
+          columns={updatedTableColumns}
+          initialState={{
+            pagination: {
+              paginationModel: {
+                pageSize: 10,
+              },
+            },
+          }}
+          //pageSizeOptions={[10]}
+        />
+      </Box>
+    </>
   );
-}
+};
+
+export default DataTable;
