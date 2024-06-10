2b2af27ec8b0e948e2f37eafa83e22afd4dab702
Tambi <tambi.asawo@verticalmotion.ca>
2024-06-10 15:21:52 -0700

    clenup

--- client/src/pages/Quiz.tsx
+++ client/src/pages/Quiz.tsx
@@ -1,13 +1,6 @@
-import React from "react";
 import QuizInterface from "../components/QuizInterface";
-import { AnswerContextProvider } from "../store/context/AnswerContext";
 const Quiz = () => {
-  return (
-    <AnswerContextProvider>
-      {" "}
-      <QuizInterface />
-    </AnswerContextProvider>
-  );
+  return <QuizInterface />;
 };
 
 export default Quiz;
