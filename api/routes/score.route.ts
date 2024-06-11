ae70d34beec8a284aa4aff7e5d8fc0609d377576
Tambi <tambi.asawo@verticalmotion.ca>
2024-06-11 15:11:28 -0700

    fix: add scors controller

--- api/routes/score.route.ts
+++ api/routes/score.route.ts
@@ -2,11 +2,13 @@ import express from "express";
 import {
   SaveScoreController,
   GetScoresController,
+  GetTopScoresController,
 } from "../controllers/score.controller";
 
 const Router = express.Router();
 
 Router.post(`/save-score`, SaveScoreController);
 Router.get(`/get-scores/:id`, GetScoresController);
+Router.get(`/get-top-scores/`, GetTopScoresController);
 
 export default Router;
