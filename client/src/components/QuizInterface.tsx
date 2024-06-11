2897b802c0348331b31af0eb10f8394c2b917b07
Tambi <tambi.asawo@verticalmotion.ca>
2024-06-11 12:03:22 -0700

    fix: encapsulate quiz component

--- client/src/components/QuizInterface.tsx
+++ client/src/components/QuizInterface.tsx
@@ -1,41 +1,42 @@
 import React from "react";
-import { useParams } from "react-router-dom";
 import { chooseAnswer, reset } from "../store/features/question/questionSlice";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../store/store";
 import QuizResult from "./QuizResult";
-import useGetQuestions, { Question } from "../hooks/useGetQuestions";
-import { useSaveScore } from "../hooks/useSaveScore";
+import { Question } from "../hooks/useQuestions";
+import { useSaveScore } from "../hooks/useScores";
+import { Skeleton } from "@mui/material";
 
 interface ResponseData {
-  results: Question[];
+  data: Question[];
+  isFetching: boolean;
+  error: Error | null;
   count: number;
 }
 
-const QuizInterface = () => {
+const QuizInterface = ({
+  responseData,
+  urlParams,
+  page,
+  nextPageHandler,
+  prevPageHandler,
+}: {
+  responseData: ResponseData;
+  urlParams: string | undefined;
+  page: number;
+  nextPageHandler: () => void;
+  prevPageHandler: () => void;
+}) => {
   const dispatch = useDispatch();
   const userId = useSelector((state: RootState) => state.auth.user?._id);
   const { answeredQuestions, questionsAnsweredCount } = useSelector(
     (state: RootState) => state.question
   );
+  const { isFetching, data, error, count } = responseData;
 
-  const [questions, setQuestions] = React.useState<Question[]>([]);
-  const { quiz_type } = useParams();
   const [totalScore, setTotalScore] = React.useState<undefined | number>();
-  const [page, setPage] = React.useState(1);
-  const {
-    data = { results: [], count: 0 },
-    isFetching,
-    error,
-  }: {
-    data: ResponseData;
-    isFetching: boolean;
-    error: Error | null;
-  } = useGetQuestions(quiz_type, page);
-  const { results, count } = data;
 
   const { mutation } = useSaveScore();
-
   let totalQuestionsCount = count || 0;
   const handleChange = (id: string, answer: string, correctAnswer: string) => {
     dispatch(chooseAnswer({ id, answer, correctAnswer }));
@@ -54,7 +55,7 @@ const QuizInterface = () => {
         scoreCount++;
       }
     }
-    const quizParamsArray = quiz_type?.split("&");
+    const quizParamsArray = urlParams?.split("&");
     const requestBody: Record<string, string> = {};
 
     for (let param of quizParamsArray as []) {
@@ -62,7 +63,7 @@ const QuizInterface = () => {
       requestBody[paramName[0]] = paramName[1] ? paramName[1] : "Any";
     }
     mutation.mutateAsync(
-      quiz_type === "quick-quiz"
+      urlParams === "quick-quiz"
         ? {
             category: "Any",
             type: "Any",
@@ -84,25 +85,29 @@ const QuizInterface = () => {
   React.useEffect(() => {
     dispatch(reset());
     setTotalScore(undefined);
-    setQuestions(results);
   }, []);
 
-  console.log({ results, questions, page, count, error });
+  if (isFetching) {
+    return (
+      <Skeleton
+        variant="rounded"
+        width={"100%"}
+        height={"100vh"}
+        animation="wave"
+        className="skeleton-class mx-auto"
+      />
+    );
+  }
+  console.log({ questionsAnsweredCount, count });
   return totalScore === undefined ? (
     <div className="rounded-lg px-3 pt-7 pb-3 bg-[#37373e] text-white ">
-      {isFetching
-        ? "Loading..."
-        : error
+      {error
         ? "Something unexpected Happened."
-        : questions.map((question, index) => {
+        : data.map((question, index) => {
             return (
               <div className="mb-7" key={question.id}>
                 <h2 className="text-lg">
-                  {questions.length * page -
-                    4 +
-                    index +
-                    ". " +
-                    question.question}
+                  {data.length * page - 4 + index + ". " + question.question}
                 </h2>
                 <div className="px-4">
                   {question.answers.map((answer) => {
@@ -142,7 +147,7 @@ const QuizInterface = () => {
         <div className=" pt-5 pb-3 space-x-3">
           {page > 1 && (
             <button
-              onClick={() => setPage((prev) => prev - 1)}
+              onClick={prevPageHandler}
               className="text-center bg-[#fe9d73] rounded-xl px-4 py-1 text-md hover:opacity-[.9] text-black disabled:bg-[#b8907e]"
               disabled={page === 1}
             >
@@ -151,7 +156,7 @@ const QuizInterface = () => {
           )}{" "}
           {page * 5 < count && (
             <button
-              onClick={() => setPage((prev) => prev + 1)}
+              onClick={nextPageHandler}
               className="text-center disabled:bg-[#b8907e] bg-[#fe9d73] rounded-xl px-4 py-1 text-md hover:opacity-[.9] text-black"
               disabled={page * 5 === count}
             >
@@ -163,7 +168,8 @@ const QuizInterface = () => {
           {page * 5 === count && (
             <button
               onClick={handleSubmitQuiz}
-              className="text-center bg-[#fe9d73] rounded-xl px-4 py-1 text-md hover:opacity-[.9] text-black"
+              className="text-center bg-[#fe9d73] disabled:bg-gray-400 disabled:cursor-not-allowed  rounded-xl px-4 py-1 text-md hover:opacity-[.9] text-black"
+              disabled={questionsAnsweredCount !== count}
             >
               Submit
             </button>
