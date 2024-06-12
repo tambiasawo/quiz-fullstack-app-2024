a2607a7d4c80eaa2c8f342d1a7a3a8169d2e46ea
Tambi <tambi.asawo@verticalmotion.ca>
2024-06-12 10:43:07 -0700

    feat: add marks controller

--- api/controllers/marks.controller.ts
+++ api/controllers/marks.controller.ts
@@ -1,13 +1,14 @@
 import Mark from "../models/Marks.model";
 
 export const SaveMarksController = async (req, res, next) => {
-  const { marks, userId, scoreCount } = req.body;
+  const { marks, userId, scoreCount, marksId } = req.body;
 
   try {
     const newMark = new Mark({
       marks,
       userId,
       scoreCount,
+      marksId,
     });
     const addedMark = await Mark.create(newMark);
     if (!addedMark) {
@@ -16,6 +17,22 @@ export const SaveMarksController = async (req, res, next) => {
       );
     }
     return res.status(201).json({ success: true, mark: addedMark });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const GetMarksController = async (req, res, next) => {
+  try {
+    const marks = await Mark.find({ userId: req.params.id });
+    if (!marks) {
+      return res.status(404).json({
+        success: false,
+        message: "No score found. You haven't taken any test yet",
+        scores: [],
+      });
+    }
+    return res.status(200).json({ success: true, results: marks });
   } catch (err) {
     next(err);
   }
