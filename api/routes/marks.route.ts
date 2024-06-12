7478ddc247be29eba21d41e1a1629982c48b32f4
Tambi <tambi.asawo@verticalmotion.ca>
2024-06-12 10:43:01 -0700

    feat: add marks route

--- api/routes/marks.route.ts
+++ api/routes/marks.route.ts
@@ -1,7 +1,12 @@
 import express from "express";
-import { SaveMarksController } from "../controllers/marks.controller";
+import {
+  GetMarksController,
+  SaveMarksController,
+} from "../controllers/marks.controller";
 
 const Router = express.Router();
 
 Router.post(`/save-marks`, SaveMarksController);
+Router.get(`/`, GetMarksController);
+
 export default Router;
