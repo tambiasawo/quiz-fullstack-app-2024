9cd134725d5691b33ffb7b3ea07643f9b74e83d6
Tambi <tambi.asawo@verticalmotion.ca>
2024-06-14 12:09:18 -0700

    feat: create usemarks query

--- client/src/hooks/useMarks.tsx
+++ client/src/hooks/useMarks.tsx
@@ -1,16 +1,8 @@
-import { QueryClient, useMutation } from "@tanstack/react-query";
+import { QueryClient, useMutation, useQuery } from "@tanstack/react-query";
 import { Marks } from "../components/QuizInterface";
 
-const saveMarks = async (params: {
-  userId?: string;
-  marks: Marks[];
-  scoreCount: number;
-  marksId: string;
-}) => {
+const saveMarks = async (params: { marks: Marks[]; marksId: string }) => {
   try {
-    if (!params.userId || !params.marksId) {
-      throw new Error();
-    }
     await fetch(`http://localhost:3000/api/marks/save-marks`, {
       method: "post",
       headers: { "Content-Type": "application/json" },
@@ -21,16 +13,38 @@ const saveMarks = async (params: {
   }
 };
 
-const useMarks = () => {
+const getMark = async (id: string) => {
+  try {
+    const response = await fetch(
+      `http://localhost:3000/api/marks/get-mark/${id}`
+    );
+    const responseData = await response.json();
+    if (responseData.success) return responseData.results[0].marks;
+    return responseData.results;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const useSaveMarks = () => {
   const queryClient = new QueryClient();
   const mutation = useMutation({
     mutationFn: saveMarks,
     onSuccess: () => {
       // Invalidate and refetch
-      queryClient.invalidateQueries({ queryKey: ["save-scores"] });
+      queryClient.invalidateQueries({ queryKey: ["save-marks"] });
     },
   });
   return { mutation };
 };
 
-export default useMarks;
+const useGetMark = (id: string) => {
+  const { data, isFetching, error, isPending } = useQuery({
+    queryKey: ["marks", id],
+    queryFn: () => getMark(id),
+  });
+
+  return { data, isFetching, error, isPending };
+};
+
+export { useSaveMarks, useGetMark };
