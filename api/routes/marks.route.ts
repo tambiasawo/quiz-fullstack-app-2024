8458a77057f6510903ef6ca524235f58674217ed
Tambi <tambi.asawo@verticalmotion.ca>
2024-06-14 12:07:27 -0700

    fix: change marks api route

--- api/routes/marks.route.ts
+++ api/routes/marks.route.ts
@@ -1,12 +1,12 @@
 import express from "express";
 import {
-  GetMarksController,
+  GetMarkController,
   SaveMarksController,
 } from "../controllers/marks.controller";
 
 const Router = express.Router();
 
 Router.post(`/save-marks`, SaveMarksController);
-Router.get(`/`, GetMarksController);
+Router.get(`/get-mark/:id`, GetMarkController);
 
 export default Router;
