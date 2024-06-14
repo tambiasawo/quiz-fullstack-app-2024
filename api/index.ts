ff68a95828a1a396d329c67842726688e25ec10c
Tambi <tambi.asawo@verticalmotion.ca>
2024-06-14 15:45:53 -0700

    chore(deplpy)

--- api/index.ts
+++ api/index.ts
@@ -6,11 +6,14 @@ import dbConnect from "./utils/dbConnect";
 import errorHandler from "./utils/errorHandler";
 import cookieParser from "cookie-parser";
 import mainRouter from "./routes";
+import path from "path";
 
 dbConnect();
 
 const PORT = 3000;
 const app = express();
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, "/client/dist")));
 
 app.use(
   cors({
@@ -25,6 +28,11 @@ app.use(cookieParser());
 app.use(`/api`, mainRouter);
 
 app.use(errorHandler);
+
+app.use("*", (req, res) => {
+  //route that doenst match our provided routes
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.listen(PORT, () => {
   console.log("listening on port " + PORT);
