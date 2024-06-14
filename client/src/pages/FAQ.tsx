582c1f470a71454b164ccf3a58860428842a260e
Tambi <tambi.asawo@verticalmotion.ca>
2024-06-14 15:07:13 -0700

    feat: add fa1

--- client/src/pages/FAQ.tsx
+++ client/src/pages/FAQ.tsx
@@ -1,10 +1,87 @@
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import Typography from "@mui/material/Typography";
+import { MdExpandMore, MdExpandLess } from "react-icons/md";
 
-const FAQ = () => {
+export default function AccordionExpandDefault() {
   return (
     <div>
-      help
+      <Accordion defaultExpanded>
+        <AccordionSummary
+          expandIcon={<MdExpandMore />}
+          aria-controls="panel1-content"
+          id="panel1-header"
+        >
+          <Typography>Expanded by default</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<MdExpandMore />}
+          aria-controls="panel2-content"
+          id="panel2-header"
+        >
+          <Typography>Header</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<MdExpandMore />}
+          aria-controls="panel2-content"
+          id="panel2-header"
+        >
+          <Typography>Header</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<MdExpandMore />}
+          aria-controls="panel2-content"
+          id="panel2-header"
+        >
+          <Typography>Header</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<MdExpandMore />}
+          aria-controls="panel2-content"
+          id="panel2-header"
+        >
+          <Typography>Header</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
     </div>
-  )
+  );
 }
-
-export default FAQ
