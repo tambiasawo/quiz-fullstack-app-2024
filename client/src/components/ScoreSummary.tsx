7088cda7e274edbf2bee440d79960a4807afd9c1
Tambi <tambi.asawo@verticalmotion.ca>
2024-06-14 14:39:29 -0700

    feat: add lowest score

--- client/src/components/ScoreSummary.tsx
+++ client/src/components/ScoreSummary.tsx
@@ -3,6 +3,12 @@ import dateFormatter from "../utils/dateFormatter";
 import { Scores } from "../hooks/useScores";
 import { Skeleton, Tooltip } from "@mui/material";
 
+type ReducedAccParamType = {
+  percentValue: number;
+  category?: string;
+  createdAt?: string;
+};
+
 const ScoreSummary = ({
   scores,
   isFetching,
@@ -13,7 +19,7 @@ const ScoreSummary = ({
   const [scoreType, setScoreType] = React.useState("recent");
 
   const bestScore = scores?.slice()?.reduce(
-    (acc: Record<string, number | string>, curr) => {
+    (acc: ReducedAccParamType, curr) => {
       const { score, questionsCount, category, createdAt } = curr;
       const percentValue = (score / questionsCount) * 100;
       if (
@@ -27,17 +33,33 @@ const ScoreSummary = ({
     { percentValue: 0 }
   );
 
+  const worstScore = scores?.slice()?.reduce(
+    (acc: ReducedAccParamType, curr) => {
+      const { score, questionsCount, category, createdAt } = curr;
+      const percentValue = (score / questionsCount) * 100;
+      if (
+        typeof acc["percentValue"] === "number" &&
+        percentValue < acc["percentValue"]
+      ) {
+        acc = { percentValue, category, createdAt };
+      }
+      return acc;
+    },
+    { percentValue: 100 }
+  );
+
   const averageScore = scores?.slice()?.reduce((acc: number, curr: Scores) => {
     const { score, questionsCount } = curr;
     const percentValue = (score / questionsCount) * 100;
     return acc + percentValue;
   }, 0);
 
-  const recentScore = scores && {
-    percentValue: (scores[0]?.score / scores[0]?.questionsCount) * 100,
-    category: scores[0]?.category,
-    createdAt: scores[0]?.createdAt,
-  };
+  const recentScore =
+    {
+      percentValue: (scores[0]?.score / scores[0]?.questionsCount) * 100,
+      category: scores[0]?.category,
+      createdAt: scores[0]?.createdAt,
+    } || "--";
 
   const displayScore =
     scoreType === "average"
@@ -52,10 +74,16 @@ const ScoreSummary = ({
           category: recentScore?.category,
           date: dateFormatter(recentScore?.createdAt),
         }
-      : {
-          value: bestScore.percentValue || "--",
+      : scoreType === "best"
+      ? {
+          value: bestScore.percentValue,
           category: bestScore.category || "--",
           date: dateFormatter(bestScore.createdAt as string) || "--",
+        }
+      : {
+          value: worstScore.percentValue,
+          category: worstScore.category || "--",
+          date: dateFormatter(worstScore.createdAt as string) || "--",
         };
 
   if (isFetching) {
@@ -82,13 +110,14 @@ const ScoreSummary = ({
           disabled={scores.length === 0}
         >
           <option value="recent">Most Recent</option>
-          <option value="best">Best</option>
+          <option value="best">Highest</option>
           <option value="average">Average</option>
+          <option value="worst">Lowest</option>
         </select>
       </div>
       <div className="max-w-lg mx-auto flex flex-col gap-1 items-center">
         <div className="bg-[#fe9d73]  w-[190px] h-[190px] rounded-full flex justify-center items-center p-3">
-          {displayScore.value ? (
+          {displayScore.value >= 0 ? (
             <h1 className="text-black text-6xl text-center ">
               {displayScore.value + "%"}
             </h1>
