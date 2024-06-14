fb1612cdc09f9c9e7c4067c0f5a22bb117a315ca
Tambi <tambi.asawo@verticalmotion.ca>
2024-06-14 12:04:24 -0700

    fix: enable login in prod

--- api/index.ts
+++ api/index.ts
@@ -9,12 +9,23 @@ import dbConnect from "./utils/dbConnect";
 import errorHandler from "./utils/errorHandler";
 import cookieParser from "cookie-parser";
 import ScoreRouter from "./routes/score.route";
+import path from "path";
+
 dbConnect();
 
 const PORT = 3000;
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PATCH", "DELETE"],
+    credentials: true,
+  })
+);
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(`/api/quiz`, QuizRouter);
@@ -25,6 +36,11 @@ app.use("/api/marks", MarksRouter);
 app.use(errorHandler);
 app.get("/", (req, res) => {
   return res.send("hello");
+});
+
+app.use("*", (req, res) => {
+  //route that doenst match our provided routes
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
 
 app.listen(PORT, () => {
