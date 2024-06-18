c99185a8affdb41146936a50b69d10a698d1ebf3
Tambi <tambi.asawo@verticalmotion.ca>
2024-06-18 10:38:10 -0700

    fix(css): change view on mobile

--- client/src/components/RootLayout.tsx
+++ client/src/components/RootLayout.tsx
@@ -11,8 +11,8 @@ export default function Root() {
   return (
     <>
       {user ? (
-        <div className="grid-cols-1 md:grid md:grid-cols-12 h-[100vh]">
-          <div className="hidden md:block md:col-span-2 border-r border-r-[#37373e] h-screen overflow-hidden">
+        <div className="grid-cols-1 md:grid lg:grid-cols-12 h-[100vh]">
+          <div className="hidden lg:block md:col-span-2 border-r border-r-[#37373e] h-screen overflow-hidden">
             <Navbar />
           </div>
           <div className="col-span-10 px-5 pb-5 h-[100vh] overflow-y-auto">
