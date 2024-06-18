8965e13291dccf2c682ffc8684579f0ee05f9414
Tambi <tambi.asawo@verticalmotion.ca>
2024-06-18 10:37:03 -0700

    fix: check token

--- api/utils/verifyToken.ts
+++ api/utils/verifyToken.ts
@@ -1,4 +1,18 @@
-export default function verifyToken(req) {
-    const token = req.params.token
-    //if(!token) return res.status(401).json(message:)
-}
+import jwt from "jsonwebtoken";
+
+export const verifyToken = async (req, res, next) => {
+  try {
+    const token = req.cookies?.access_token;
+    if (!token) {
+      return res.status(401).json("You are not authorized to access this");
+    }
+
+    jwt.verify(token, process.env.PRIVATE_KEY as string, (err, user) => {
+      if (err) return res.status(403).json("Your token is invalid");
+      req.user = user;
+      next();
+    });
+  } catch (err) {
+    res.status(500).json("Something went wrong: " + err.message);
+  }
+};
