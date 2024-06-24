4e81fe9f9471b6e7d2df4671ceb8c82d66ca8f78
Tambi <tambi.asawo@verticalmotion.ca>
2024-06-24 14:53:55 -0700

    cleanup

--- client/src/pages/Results.tsx
+++ client/src/pages/Results.tsx
@@ -89,7 +89,7 @@ const Results = () => {
   const columns = React.useMemo(() => {
     return [...cols, actionColumn];
   }, []);
-  console.log({ quizMarks });
+
   return (
     <div className="space-y-7">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
